7046ca32d9f4c0bf8c84b9f7853ac7bb45ffc5f2
Viacheslav Ryabov <vermulion@gmail.com>
2022-11-01 14:33:30 +0300

    First balance stabilizer was added

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -37,7 +37,7 @@ public class DataHelper {
   }
 
   public static cardInfo validCard2 () {
-    return new cardInfo("5559 0000 0000 0001", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    return new cardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
   }
 
   public static cardInfo notValidCard () {
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,14 +1,16 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
 import ru.netology.web.page.MoneyTransferPage;
+import org.junit.jupiter.api.Nested;
 
 
 import static com.codeborne.selenide.Selenide.open;
@@ -22,58 +24,112 @@ class MoneyTransferTest {
 
   }
 
-  @AfterEach
-  @DisplayName("Return the balance to its original position")
-  void returnAccBalanceToOriginalState() {
-    DashboardPage dashboardPage = new DashboardPage();
-    int balance = 10000;
-    int cardBalance1 = dashboardPage.getBalance(DataHelper.validCard1().getDataTestId());
-    int cardBalance2 = dashboardPage.getBalance(DataHelper.validCard2().getDataTestId());
-    int remains;
-    if (cardBalance1 > balance) {
-      remains = cardBalance1 - balance;
-      dashboardPage.secondCard();
-      var transfer = new MoneyTransferPage();
+  @Nested
+  public class forHappyPaths {
+    @AfterEach
+    void tearDown() {
+      Selenide.closeWebDriver();
+    }
+
+    @AfterEach
+    @DisplayName("Return the balance to its original position")
+    void returnAccBalanceToOriginalState() {
+      open("http://localhost:9999");
+      var LoginPage = new LoginPage();
+      var authInfo = DataHelper.getAuthInfo();
+      var verificationPage = LoginPage.validLogin(authInfo);
+      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+      DashboardPage dashboardPage = new DashboardPage();
+      int balance = 10000;
+      int cardBalance1 = dashboardPage.getBalance(DataHelper.validCard1().getDataTestId());
+      int cardBalance2 = dashboardPage.getBalance(DataHelper.validCard2().getDataTestId());
+      int remains;
+      if (cardBalance1 > balance) {
+        remains = cardBalance1 - balance;
+        dashboardPage.secondCard();
+        var transfer = new MoneyTransferPage();
+        var idCard = DataHelper.validCard1();
+        transfer.transfer(remains, idCard.getCardNumber());
+      }
+      if (cardBalance2 > balance) {
+        remains = cardBalance2 - balance;
+        dashboardPage.firstCard();
+        var transfer = new MoneyTransferPage();
+        var idCard = DataHelper.validCard2();
+        transfer.transfer(remains, idCard.getCardNumber());
+      }
+    }
+
+
+    @Test
+    void shouldTransferMinimumMoneyFrom1to2Card() {
+      int transferSum = 1;
+      var LoginPage = new LoginPage();
+      var authInfo = DataHelper.getAuthInfo();
+      var verificationPage = LoginPage.validLogin(authInfo);
+      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+      var dashBoardPage = new DashboardPage();
+      int expBalance1 = dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()) - transferSum;
+      int expBalance2 = dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()) + transferSum;
+      var moneyTransferPage = dashBoardPage.secondCard();
       var idCard = DataHelper.validCard1();
-      transfer.transfer(remains, idCard.getCardNumber());
+      moneyTransferPage.transfer(transferSum, idCard.getCardNumber());
+      assertEquals(expBalance1, dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()));
+      assertEquals(expBalance2, dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()));
     }
-    if (cardBalance2 > balance) {
-      remains = cardBalance2 - balance;
-      dashboardPage.firstCard();
-      var transfer = new MoneyTransferPage();
+
+    @Test
+    void shouldTransferBoundaryValueOfMoneyFrom2to1Card() {
+      int transferSum = 9999;
+      var LoginPage = new LoginPage();
+      var authInfo = DataHelper.getAuthInfo();
+      var verificationPage = LoginPage.validLogin(authInfo);
+      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+      var dashBoardPage = new DashboardPage();
+      int expBalance1 = dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()) - transferSum;
+      int expBalance2 = dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()) + transferSum;
+      var moneyTransferPage = dashBoardPage.secondCard();
+      var idCard = DataHelper.validCard1();
+      moneyTransferPage.transfer(transferSum, idCard.getCardNumber());
+      assertEquals(expBalance1, dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()));
+      assertEquals(expBalance2, dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()));
+    }
+
+    @Test
+    void shouldTransferAllMoneyFrom2to1Card() {
+      int transferSum = 10000;
+      var LoginPage = new LoginPage();
+      var authInfo = DataHelper.getAuthInfo();
+      var verificationPage = LoginPage.validLogin(authInfo);
+      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+      var dashBoardPage = new DashboardPage();
+      int expBalance1 = dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()) - transferSum;
+      int expBalance2 = dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()) + transferSum;
+      var moneyTransferPage = dashBoardPage.secondCard();
+      var idCard = DataHelper.validCard1();
+      moneyTransferPage.transfer(transferSum, idCard.getCardNumber());
+      assertEquals(expBalance1, dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()));
+      assertEquals(expBalance2, dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()));
+    }
+  }
+
+    @Test
+    void shouldTransferMoneyToTheSameCard() {
+      int transferSum = 100;
+      var LoginPage = new LoginPage();
+      var authInfo = DataHelper.getAuthInfo();
+      var verificationPage = LoginPage.validLogin(authInfo);
+      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+      var dashBoardPage = new DashboardPage();
+      var moneyTransferPage = dashBoardPage.secondCard();
       var idCard = DataHelper.validCard2();
-      transfer.transfer(remains, idCard.getCardNumber());
-    }
+      moneyTransferPage.transfer(transferSum, idCard.getCardNumber());
 
   }
-
-
-  @Test
-  void shouldTransferMinimumMoneyFrom1to2Card() {
-    int transferSum = 1;
-    var LoginPage = new LoginPage();
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = LoginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-    var dashBoardPage = new DashboardPage();
-    int expBalance1 = dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()) - transferSum;
-    int expBalance2 = dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()) + transferSum;
-    var moneyTransferPage = dashBoardPage.secondCard();
-    var idCard = DataHelper.validCard1();
-    moneyTransferPage.transfer(transferSum, idCard.getCardNumber());
-    assertEquals(expBalance1, dashBoardPage.getBalance(DataHelper.validCard1().getDataTestId()));
-    assertEquals(expBalance2, dashBoardPage.getBalance(DataHelper.validCard2().getDataTestId()));
-
-  }
-
-//  @Test
-//  void shouldTransferMoneyBetweenOwnCardsV3() {
-//    var loginPage = open("http://localhost:9999", LoginPageV3.class);
-//    var authInfo = DataHelper.getAuthInfo();
-//    var verificationPage = loginPage.validLogin(authInfo);
-//    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-//    verificationPage.validVerify(verificationCode);
-//  }
 }
 
